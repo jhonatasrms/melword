3480b0c4ba84d0887a585d2865ffce69118e411f
Lojawohtech <102686926+jhonatasrms@users.noreply.github.com>
2023-09-16 13:36:01 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -32,5 +32,5 @@ const loop= setInterval(()=> {
 
     }, 10);
 
-document.addEventListener('keydown', jump)
+document.addEventListener('touchstart', jump)
 document.addEventListener('touchstart', jump);
